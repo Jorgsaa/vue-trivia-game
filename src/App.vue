abccc604430c7635f77523c091fc2311144e36fd
Jonas Kristoffersen <jo_nas@live.no>
2022-01-24 16:48:50 +0100

    feat(layout): Implemented common layout for all routes: the header, and main element

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,50 @@
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>GitHub Pages test</h1>
-  <router-link to="/">Start</router-link>
-  <router-link to="/question">Question</router-link>
-  <router-link to="/result">Result</router-link>
-  <router-view />
+  <div class="vertical-layout">
+    <header>
+      <h1>Trivia</h1>
+      Temporary route links:
+      <router-link to="/">Start</router-link>
+      <router-link to="/question">Question</router-link>
+      <router-link to="/result">Result</router-link>
+    </header>
+    <main class="middle">
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    padding: 0px;
+    margin: 0px;
+
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+</style>
+
+<style scoped>
+  header {
+    text-align: center;
+    background-color: gray;
+    height: 10%;
+  }
+
+  .vertical-layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .middle {
+    flex: 1;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
