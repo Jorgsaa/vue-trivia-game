845b58e32498a6827adda4282f6dc7e977e3eb64
JonasBM <jonas.mela@gmail.com>
2022-01-24 17:22:57 +0100

    REFACTOR: add component folder

---
+++ src/components/Question.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  question: {
+      type: Object,
+      required: true,
+  }
+})
+
+const choice1 = "Choice 1"
+const choice2 = "Choice 2"
+const choice3 = "Choice 3"
+const choice4 = "Choice 4"
+</script>
+
+
+<template>
+<div>
+    <!-- <p>{{ numberOfCurrentQuestion }}/{{ numberOfQuestions }}</p> -->
+    <h2>{{ question.question }}</h2>
+</div>
+<button @click="nextQuestion(choice1)">{{ choice1 }}</button>
+<button @click="nextQuestion(choice2)">{{ choice2 }}</button>
+<button @click="nextQuestion(choice3)">{{ choice3 }}</button>
+<button @click="nextQuestion(choice4)">{{ choice4 }}</button>
+</template>
+
+
+<style scoped>
+
+</style>
