c0444c03490eeb297c561b4d29e47220ba698c77
JonasBM <jonas.mela@gmail.com>
2022-01-24 19:19:20 +0100

    FEAT: submit answer

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -2,10 +2,10 @@
 <div>
     <h2>{{ question.question }}</h2>
 </div>
-<button @click="submitAnswer(choice1, question)">{{ choice1 }}</button>
-<button @click="submitAnswer(choice2, question)">{{ choice2 }}</button>
-<button @click="submitAnswer(choice3, question)">{{ choice3 }}</button>
-<button @click="submitAnswer(choice4, question)">{{ choice4 }}</button>
+<button @click="submitAnswer(choice1, question, answers)">{{ choice1 }}</button>
+<button @click="submitAnswer(choice2, question, answers)">{{ choice2 }}</button>
+<button @click="submitAnswer(choice3, question, answers)">{{ choice3 }}</button>
+<button @click="submitAnswer(choice4, question, answers)">{{ choice4 }}</button>
 </template>
 
 
@@ -13,6 +13,10 @@
 defineProps({
   question: {
       type: Object,
+      required: true,
+  },
+  answers: {
+      type: Array,
       required: true,
   }
 })
@@ -27,11 +31,12 @@ const randomizeChoices = () => {
     console.log("Coices randomized")
 }
 
-const emit = defineEmits(['submit-answer'])
+const emit = defineEmits(['next-question'])
 
-const submitAnswer = (choice, question) => {
+const submitAnswer = (choice, question, answers) => {
     question.show_question = false
-    
+    answers.push({"question":question.question, "correct_answer":question.correct_answer, "answer":choice, "number":question.number})
+    emit('next-question')
 }
 </script>
 
--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -2,7 +2,7 @@
     <h1>Question</h1>
     <!-- <p>{{ numberOfCurrentQuestion }}/{{ numberOfQuestions }}</p> -->
     <div v-for="question in questions" :key="question.question">
-        <Question :question="question" v-if="question.show_question"/>
+        <Question :question="question" :answers="answers" v-if="question.show_question" @next-question="nextQuestion" />
     </div>
     <button @click="debug">Debug</button>
 </template>
@@ -20,24 +20,20 @@ const debug = () => {
 
 // Hardcoded questions
 const questions = reactive([
-    {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"Which of the following movies was not based on a novel by Stephen King? ","correct_answer":"The Thing","incorrect_answers":["Carrie","Misery","The Green Mile"],"show_question":true},
-    {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"Who is the main protagonist in, the 1985 film, Back to the Future?","correct_answer":"Marty McFly","incorrect_answers":["Emmett &quot;Doc&quot; Brown","Biff Tannen","George McFly"],"show_question":false},
-    {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"In the 1995 film &quot;Balto&quot;, who are Steele&#039;s accomplices?","correct_answer":"Kaltag, Nikki, and Star","incorrect_answers":["Dusty, Kirby, and Ralph","Nuk, Yak, and Sumac","Jenna, Sylvie, and Dixie"],"show_question":false}
+    {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"Which of the following movies was not based on a novel by Stephen King? ","correct_answer":"The Thing","incorrect_answers":["Carrie","Misery","The Green Mile"],"show_question":true,"number":"1"},
+    {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"Who is the main protagonist in, the 1985 film, Back to the Future?","correct_answer":"Marty McFly","incorrect_answers":["Emmett &quot;Doc&quot; Brown","Biff Tannen","George McFly"],"show_question":false,"number":"2"},
+    {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"In the 1995 film &quot;Balto&quot;, who are Steele&#039;s accomplices?","correct_answer":"Kaltag, Nikki, and Star","incorrect_answers":["Dusty, Kirby, and Ralph","Nuk, Yak, and Sumac","Jenna, Sylvie, and Dixie"],"show_question":false,"number":"3"},
 ])
-const numberOfQuestions = questions.length
-const numberOfCurrentQuestion = ref(0)
+const answers = []
 
-const answers = [
-    {"question":"hans?", "correct_answer":"flame", "answer":"water", "number":"1", "showQuestion":"false"},
-    {"question":"hans?", "correct_answer":"flame", "answer":"water", "number":"2", "showQuestion":"true"},
-    {"question":"hans?", "correct_answer":"flame", "answer":"water", "number":"3", "showQuestion":"false"}
-]
+const numberOfQuestions = questions.length
+const numberOfCurrentQuestion = ref(1)
 
 
 const nextQuestion = () => {
     numberOfCurrentQuestion.value++
+    questions[numberOfCurrentQuestion.value].show_question = true
     console.log(numberOfCurrentQuestion.value)
-    console.log(choice)
 }
 
 </script>
