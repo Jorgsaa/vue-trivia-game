d725a0f1155d42bb0f2bcc9a33b791d3434dcd7b
Jonas Kristoffersen <jo_nas@live.no>
2022-01-24 20:16:44 +0100

    feat(resultpage): Defined HTML/CSS layout of result page

---
+++ src/components/ResultElement.vue
@@ -0,0 +1,14 @@
+<template>
+    <li>
+        <!-- Insert data -->
+    </li>
+</template>
+
+<script></script>
+
+<style scoped>
+    li {
+        background-color: violet;
+        height: 50px;
+    }
+</style>
---
+++ src/components/ResultList.vue
@@ -0,0 +1,21 @@
+<template>
+    <ol>
+        <ResultElement />
+    </ol>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+    ol {
+        box-sizing: border-box;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-auto-rows: max-content;
+        overflow: hidden;
+        overflow-y: scroll;
+        gap: 5px;
+    }
+</style>
