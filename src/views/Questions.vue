c183fefaa05a59b306b773d2ff0c6df16c237529
JonasBM <jonas.mela@gmail.com>
2022-01-24 18:22:43 +0100

    REFACTOR: questions and question code

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,5 +1,6 @@
 <template>
     <h1>Question</h1>
+    <!-- <p>{{ numberOfCurrentQuestion }}/{{ numberOfQuestions }}</p> -->
     <div v-for="question in questions" :key="question.question">
         <Question :question="question" v-if="question.show_question"/>
     </div>
@@ -8,21 +9,14 @@
 
 
 <script setup>
-import { ref, onMounted, reactive } from "vue";
+import { ref, reactive } from "vue";
 import Question from '../components/Question.vue'
 
 const debug = () => {
     console.log(questions[1].show_question)
+    questions[0].show_question = false
+    questions[1].show_question = true
 }
-
-const che = () => {
-    //question.question === questions[indexOfCurrentQuestion].question
-    return false;
-}
-
-onMounted(() => {
-    randomizeChoices()
-})
 
 // Hardcoded questions
 const questions = reactive([
@@ -30,45 +24,20 @@ const questions = reactive([
     {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"Who is the main protagonist in, the 1985 film, Back to the Future?","correct_answer":"Marty McFly","incorrect_answers":["Emmett &quot;Doc&quot; Brown","Biff Tannen","George McFly"],"show_question":false},
     {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"In the 1995 film &quot;Balto&quot;, who are Steele&#039;s accomplices?","correct_answer":"Kaltag, Nikki, and Star","incorrect_answers":["Dusty, Kirby, and Ralph","Nuk, Yak, and Sumac","Jenna, Sylvie, and Dixie"],"show_question":false}
 ])
-const numberOfQuestions = questions.length;
-const numberOfCurrentQuestion = ref(1)
-const indexOfCurrentQuestion = 0
+const numberOfQuestions = questions.length
+const numberOfCurrentQuestion = ref(0)
 
 const answers = [
     {"question":"hans?", "correct_answer":"flame", "answer":"water", "number":"1", "showQuestion":"false"},
     {"question":"hans?", "correct_answer":"flame", "answer":"water", "number":"2", "showQuestion":"true"},
     {"question":"hans?", "correct_answer":"flame", "answer":"water", "number":"3", "showQuestion":"false"}
 ]
-/* const questions = []
-for (let index = 0; index < questionsList.length; index++) {
-    questions.push({
-        questionData: questionsList[index],
-        number: index+1,
-        index: index
-    })
-    
-} */
-
-const choice1 = "Choice 1"
-const choice2 = "Choice 2"
-const choice3 = "Choice 3"
-const choice4 = "Choice 4"
 
 
-const getRandomChoice = () => {
-    return "testchoice"
-}
-
-const nextQuestion = (choice, e) => {
+const nextQuestion = () => {
     numberOfCurrentQuestion.value++
     console.log(numberOfCurrentQuestion.value)
     console.log(choice)
 }
-
-const randomizeChoices = () => {
-
-    console.log("Coices randomized")
-}
-
 
 </script>
