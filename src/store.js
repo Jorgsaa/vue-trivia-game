27edba92fda1f78bdcb12a4cd79bf7387cc80fd5
JonasBM <jonas.mela@gmail.com>
2022-01-25 10:40:11 +0100

    FEAT: questions and answers state

--- src/store.js
+++ src/store.js
@@ -2,21 +2,43 @@
  * Dependencies
  * @ignore
  */
- import { createStore } from 'vuex'
+import { createStore } from 'vuex'
 
- /**
-  * Store
-  * @ignore
-  */
- const store = createStore({
-     state: {
-     },
-     mutations: {
-     },
- })
- 
- /**
-  * Exports
-  * @ignore
-  */
- export default store
+/**
+ * Store
+ * @ignore
+ */
+const store = createStore({
+    state: {
+        questions: [],
+        answers: [],
+    },
+    mutations: {
+        addQuestion: (state, payload) => {
+            state.questions.push(payload)
+        },
+        deleteQuestion: (state, payload) => {
+            const index = state.questions.indexOf(payload)
+            state.questions.splice(index, 1)
+        },
+        emptyQuestions: (state) => {
+            state.questions.length = 0
+        },
+        submitAnswer: (state, payload) => {
+            state.answers.push(payload)
+        },
+        deleteAnswer: (state, payload) => {
+            const index = state.answers.indexOf(payload)
+            state.answers.splice(index, 1)
+        },
+        emptyAnswers: (state) => {
+            state.answers.length = 0
+        },
+    },
+})
+
+/**
+ * Exports
+ * @ignore
+ */
+export default store
