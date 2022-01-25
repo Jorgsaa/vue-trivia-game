53a53defce674fdb2e28c7a28c1696a3b5ab7f36
JonasBM <jonas.mela@gmail.com>
2022-01-25 11:21:02 +0100

    FEAT: questions get data from store

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -7,18 +7,21 @@
 
 
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 import Question from '../components/Question.vue'
 import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 
-    const router = useRouter();
+const store = useStore()
+const router = useRouter();
 
 // Hardcoded questions
-const questions = reactive([
+const questions = computed(() => store.state.questions)
+/* const questions = reactive([
     {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"Which of the following movies was not based on a novel by Stephen King? ","correct_answer":"The Thing","incorrect_answers":["Carrie","Misery","The Green Mile"],"show_question":true,"number":"1"},
     {"category":"Entertainment: Film","type":"boolean","difficulty":"easy","question":"The sky is blue","correct_answer":"True","incorrect_answers":["False"],"show_question":false,"number":"2"},
     {"category":"Entertainment: Film","type":"multiple","difficulty":"easy","question":"In the 1995 film &quot;Balto&quot;, who are Steele&#039;s accomplices?","correct_answer":"Kaltag, Nikki, and Star","incorrect_answers":["Dusty, Kirby, and Ralph","Nuk, Yak, and Sumac","Jenna, Sylvie, and Dixie"],"show_question":false,"number":"3"},
-])
+]) */
 const answers = []
 const indexOfCurrentQuestion = ref(0)
 
@@ -26,8 +29,8 @@ const indexOfCurrentQuestion = ref(0)
 const nextQuestion = () => {
     console.log(answers[indexOfCurrentQuestion.value])
     indexOfCurrentQuestion.value++
-    if (indexOfCurrentQuestion.value < questions.length) {
-        questions[indexOfCurrentQuestion.value].show_question = true 
+    if (indexOfCurrentQuestion.value < questions.value.length) {
+        questions.value[indexOfCurrentQuestion.value].show_question = true 
     } else {
         router.push({name: 'result'})
     }
