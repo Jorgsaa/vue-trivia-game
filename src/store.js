c06d125f43d1b59f9d62e8e874a4f9c9069b4f06
JonasBM <jonas.mela@gmail.com>
2022-01-25 10:13:31 +0100

    CHORE(store): create file

---
+++ src/store.js
@@ -0,0 +1,22 @@
+/**
+ * Dependencies
+ * @ignore
+ */
+ import { createStore } from 'vuex'
+
+ /**
+  * Store
+  * @ignore
+  */
+ const store = createStore({
+     state: {
+     },
+     mutations: {
+     },
+ })
+ 
+ /**
+  * Exports
+  * @ignore
+  */
+ export default store
